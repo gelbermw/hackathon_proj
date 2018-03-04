ffafed1ed75cdae022d58582e902a6557abd3728
Matthew Gelber <gelbermw@vcu.edu>
2018-03-04 02:45:13 -0500

    added target pages fo participant and volunteer

---
+++ app/src/main/java/hackathon/me/hackathon_proj/SearchResultsV.java
@@ -0,0 +1,13 @@
+package hackathon.me.hackathon_proj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultsV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results_v);
+    }
+}
