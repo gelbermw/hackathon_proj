4259c321223ed9c7d10754d18312964faa6a5584
Lauren <l.c.linkous@gmail.com>
2018-03-03 23:23:47 -0500

    spinner?

--- app/src/main/java/hackathon/me/hackathon_proj/ParticipantPage.java
+++ app/src/main/java/hackathon/me/hackathon_proj/ParticipantPage.java
@@ -2,12 +2,41 @@ package hackathon.me.hackathon_proj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ParticipantPage extends AppCompatActivity {
+public class ParticipantPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    String[] bankNames={"Item1","Item2","Item3","Item4","Item5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participant_page);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.Participant_spinner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,bankNames);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+}
+
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), bankNames[position], Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+// TODO Auto-generated method stub
+
     }
 }
