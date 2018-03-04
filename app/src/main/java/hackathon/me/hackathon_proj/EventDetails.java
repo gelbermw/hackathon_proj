d6fbf8cb50f787c0eceedc7d4e79df547f8fb495
Matthew Gelber <gelbermw@vcu.edu>
2018-03-04 08:24:30 -0500

    added event summary page

--- app/src/main/java/hackathon/me/hackathon_proj/EventDetails.java
+++ app/src/main/java/hackathon/me/hackathon_proj/EventDetails.java
@@ -1,7 +1,10 @@
 package hackathon.me.hackathon_proj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import hackathon.me.hackathon_proj.database.data.EventData;
 
@@ -11,6 +14,15 @@ public class EventDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        /**Home button from event details**/
+        Button participantScreen = findViewById(R.id.to_participant);
+        participantScreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(EventDetails.this, MainActivity.class);
+                EventDetails.this.startActivity(myIntent);
+            }
+        });
     }
 }
 
