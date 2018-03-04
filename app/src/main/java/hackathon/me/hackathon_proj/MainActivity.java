3e664492491c8885eedb0d58fbc33089dbff1431
Matthew Gelber <gelbermw@vcu.edu>
2018-03-03 19:37:05 -0500

    participant button

--- app/src/main/java/hackathon/me/hackathon_proj/MainActivity.java
+++ app/src/main/java/hackathon/me/hackathon_proj/MainActivity.java
@@ -1,7 +1,10 @@
 package hackathon.me.hackathon_proj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button participantScreen = (Button) findViewById(R.id.to_participant);
+
+        participantScreen.setOnClickListener(new View.OnClickListener() {           //Sets an onClickListener to make button click for intent
+            public void onClick(View view) {
+
+                Intent myIntent = new Intent(MainActivity.this, ParticipantPage.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/hackathon/me/hackathon_proj/ParticipantPage.java
@@ -0,0 +1,13 @@
+package hackathon.me.hackathon_proj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParticipantPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participant_page);
+    }
+}
